b8d3c8dc3da25bdd91f3a321a87d0a9bfe1b556d
Kenny Yoon <ghun.yoon@gmail.com>
2021-05-17 18:04:14 -0400

    commit tree e8597f659d905f668612d1545aa6bc96febf8e7a

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
 
-// Write your code here
+const random = Number.parseInt((Math.random() * 100), 10);
+
+let num3 = 9
+let num4 = 5
+const mod = (num3 % num4);
+
+
+const max = Math.max(1,2,3,20);
